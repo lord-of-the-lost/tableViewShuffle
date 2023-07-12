9bc6e6acef8a1cdf3712ab89cb9236190231d4c1
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-12 12:19:06 +0300

    Initial Commit

---
+++ TableViewShuffle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewShuffle
+//
+//  Created by Николай Игнатов on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
